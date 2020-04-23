ec0c7200c08524029335dcc0bd1d7aa26ecb69b3
dhirajluthra <dhiraj_luthra@yahoo.com>
2020-04-23 15:34:49 -0700

    first commit

---
+++ src/sample/InitializeDriver.java
@@ -0,0 +1,31 @@
+package sample;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class InitializeDriver {
+	
+
+	@Test
+	public static void BrowserInitialize() {
+
+		String basePath = new File("").getAbsolutePath();
+	    System.out.println(basePath);
+
+	    String path = new File("src/main/resources/conf.properties")
+	                                                           .getAbsolutePath();
+	    System.out.println(path);
+		System.setProperty("webdriver.chrome.driver", "C://Selenium//chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
+		System.out.println("Page You Visited is: " + driver.getTitle());
+
+	}
+
+}
